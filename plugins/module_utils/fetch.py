30f23ce7a5db2566a019a6d8b30e061c647be063
Jake <jakemgsmith@gmail.com>
2022-02-01 12:24:12 +0000

    strip out comments from yang before finding imports

--- plugins/module_utils/fetch.py
+++ plugins/module_utils/fetch.py
@@ -129,7 +129,7 @@ class SchemaStore(object):
         if found:
             result["fetched"][schema_id] = data_model
             importre = re.compile(r"import (\S+) {")
-            all_found = re.sub(r"description\s+\"[^\"]+\";", "", data_model)
+            all_found = re.sub(r"/\*(.|\n)*?\*/|\/\/.*|description\s+\"[^\"]+\";", "", data_model)
             all_found = importre.findall(all_found)
             all_found = [re.sub("['\"]", "", imp) for imp in all_found]
 
